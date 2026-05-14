6c8cc9e87366c8683de14efa1a9c8c83625c2eca
Jaitin <jaitinmoodally@outlook.com>
2026-05-14 12:07:23 +0200

    another fix for wsl issue

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -13,9 +13,11 @@ export default defineConfig({
   server: {
     port: 3000,           // desired port
     strictPort: false,    // falls back to next available if 3000 is taken
-  },
+
     watch: {
       usePolling: true,
       interval: 100,   // milliseconds between polls (optional)
     },
+  },
+    
 })
