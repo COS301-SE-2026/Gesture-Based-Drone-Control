c3ad3d9d1f9e11019a72073c9c4ff385c7d1dc56
Ayush-B99 <ayush.beekum@gmail.com>
2026-05-18 18:56:41 +0200

    hand detection coding checkpoint

--- services/cv_pipeline/hand-detection/mediapipe_detector.py
+++ services/cv_pipeline/hand-detection/mediapipe_detector.py
@@ -1,1 +1,50 @@
 # /services/cv-pipeline/hand-detection/mediapipe_detector.py
+
+"""
+Hand detection using MediaPipe and opencv
+-> Takes captured frame from camera_feed.py
+-> Returns hand detection result with landmarks, max 2 hands
+-> Gesture interpretation done in recoginsers
+"""
+
+import logging
+from dataclasses import dataclass, field
+from enum import Enum, auto
+from typing import Optional
+
+import mediapipe as mp
+import numpy as np
+
+# camera_feed.py imports -> 1st in chain then this file
+from camera_feed import CaptureFrame
+
+logger = logging.getLogger(__name__)
+
+# hands logic
+_mp_hands = mp.solutions.hands
+_mp_drawing = mp.solutions.drawing_utils
+
+#consts
+#points on one hand = 21
+NUM_LANDMARKS = 21
+MAX_HANDS = 2
+
+#Enums 
+class Handedness(Enum):
+    LEFT = auto()
+    RIGHT = auto()
+    
+#Data classes
+
+@dataclass 
+class HandLandmark:
+    """
+    A single landmark point from mp
+    x, y normalised to [0.0, 1.0] relative to frame dimesions
+    z represents depth relative to wrist 
+    - val = closer to cam
+    + val = further from cam
+    """
+
+
+
