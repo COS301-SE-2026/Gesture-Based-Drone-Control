235110408cca89b376abecb8d7e0e39721379e20
Jaitin <jaitinmoodally@outlook.com>
2026-05-21 17:25:07 +0200

    flight summary drone endpoint

--- apps/backend/app/api/api.py
+++ apps/backend/app/api/api.py
@@ -35,3 +35,16 @@ async def sim_telemetry(websocket: WebSocket):
 			await asyncio.sleep(0.5)
 	except WebSocketDisconnect:
 		print('TELEMETRY CLIENT DISCONNECT')
+
+@router.get('/drone/flight-summary')
+async def flight_summary():
+	return [
+        {"flight_id": 1, "time": 20, "max_altitude": 4, "average_speed": 2.5},
+        {"flight_id": 2, "time": 17, "max_altitude": 4, "average_speed": 2.5},
+        {"flight_id": 3, "time": 25, "max_altitude": 4, "average_speed": 2.5},
+        {"flight_id": 4, "time": 18, "max_altitude": 4, "average_speed": 2.5},
+        {"flight_id": 5, "time": 21, "max_altitude": 4, "average_speed": 2.5},
+        {"flight_id": 6, "time": 19, "max_altitude": 4, "average_speed": 2.5},
+        {"flight_id": 7, "time": 22, "max_altitude": 4, "average_speed": 2.5},
+        {"flight_id": 8, "time": 20, "max_altitude": 4, "average_speed": 2.5},
+    ]
