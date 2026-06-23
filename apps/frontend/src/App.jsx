b393f82d62d364ad76f54161583fb57a054a1b75
Chinmayi Santhosh <santhoshchinmayi@gmail.com>
2026-06-23 15:10:18 +0200

    settings have the drine info card (moved from home page to settings)

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -14,6 +14,7 @@ import { ThemeProvider } from "./context/ThemeProvider.jsx"
 
 function App() {
   return (
+    
     <ThemeProvider>
       <Routes>
         <Route path="/login" element={<Login />} />
