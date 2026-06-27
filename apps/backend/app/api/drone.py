011f8e7088c64b3f426c30e7875e86bc29d37219
Shavir <shavirvallabh05@gmail.com>
2026-06-27 21:22:13 +0200

    api refactor - main boilerplate

--- apps/backend/app/api/drone.py
+++ apps/backend/app/api/drone.py
@@ -5,3 +5,8 @@ All drone routes, REST and WebSockets
 
 <include a summary here>
 """
+
+from fastapi import APIRouter
+
+
+router = APIRouter()
--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -1,3 +1,38 @@
 # /apps/backend/app/main.py
+
+"""
+Entry point for FastAPI
+
+- Create FastAPI app
+- Manage startup and shutdown
+- Mount the router
+- Store AppState so routes can access
+
+"""
+from __future__ import annotations # prevents typeerrors
+
+import logging
 import sys
 from pathlib import Path
+from contextlib import asynccontextmanager
+from pathlib import Path
+from fastapi import FastAPI
+from app.api import router
+
+logger = logging.getLogger(__name__)
+
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+  logger.info('Starting API...')
+  yield
+  logger.info('Stopping api')
+  
+
+app = FastAPI(title="GBDC API", version="1.0",
+              description="""
+              Describe each endpoint here
+              """,
+              lifespan=lifespan,)
+
+app.include_router(router)
+
