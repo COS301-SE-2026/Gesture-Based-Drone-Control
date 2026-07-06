3f8b065e3ec2fd0ab68c6ad1931674d498fc0c3d
Wave2055 <jaitinmoodally@outlook.com>
2026-07-06 15:35:09 +0200

    drone table to sqlalchemy object

---
+++ services/database_manager/models/drones.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Boolean, String, text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from services.database_manager.database import Base
+
+class Drone(Base):
+    __tablename__= "drones"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    display_name: Mapped[str] = mapped_column(String, nullable=False)
+    is_simulated: Mapped[bool] = mapped_column(Boolean, nullable=False)
