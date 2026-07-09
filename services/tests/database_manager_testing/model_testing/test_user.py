b09f8e53005e26baf29411de0dda69071a730951
Wave2055 <jaitinmoodally@outlook.com>
2026-07-09 22:53:13 +0200

    sonarqube check

--- services/tests/database_manager_testing/model_testing/test_user.py
+++ services/tests/database_manager_testing/model_testing/test_user.py
@@ -7,7 +7,7 @@ from services.database_manager.models.users import User
 
 
 async def test_create_user_minimal_fields(session):
-	# NOSONAR
+	#NOSONAR
 	user = User(email='test@example.com', hashed_password='hashed')
 	session.add(user)
 	await session.commit()
@@ -19,32 +19,32 @@ async def test_create_user_minimal_fields(session):
 
 
 async def test_email_uniqueness_enforced(session):
-	# NOSONAR
+	#NOSONAR
 	session.add(User(email='test@example.com', hashed_password='hashed'))
 	await session.commit()
 
-	# NOSONAR
+	#NOSONAR
 	session.add(User(email='test@example.com', hashed_password='hashed'))
 	with pytest.raises(IntegrityError):
 		await session.commit()
 
 
 async def test_email_required(session):
-	# NOSONAR
+	#NOSONAR
 	session.add(User(hashed_password='a'))
 	with pytest.raises(IntegrityError):
 		await session.commit()
 
 
 async def test_password_required(session):
-	# NOSONAR
+	#NOSONAR
 	session.add(User(email='example@email.com'))
 	with pytest.raises(IntegrityError):
 		await session.commit()
 
 
 async def test_is_active_can_be_overriden(session):
-	# NOSONAR
+	#NOSONAR
 	user = User(email='test@example.com', hashed_password='hashed', is_active=False)
 	session.add(user)
 	await session.commit()
@@ -52,7 +52,7 @@ async def test_is_active_can_be_overriden(session):
 
 
 async def test_flight_summaries_relationship_empty_by_default(session):
-	# NOSONAR
+	#NOSONAR
 	user = User(email='test@example.com', hashed_password='hashed', is_active=False)
 	session.add(user)
 	await session.commit()
