38cfeae47bb97278e3bce331e50886a0a781292e
ChinmayiSanthosh <santhoshchinmayi@gmail.com>
2026-07-09 14:07:58 +0200

    teeny tiny big fix

--- apps/frontend/src/hooks/useForm.js
+++ apps/frontend/src/hooks/useForm.js
@@ -27,7 +27,7 @@ export function useForm(initialState, onSuccess) {
     }
     setIsLoading(true)
     setErrors({})
-    console.log("AOI_BASE_URL is:", API_BASE_URL)
+    
     try {
       const response = await fetch(`${API_BASE_URL}/auth/login`, {
         method: "POST",
