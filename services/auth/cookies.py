02ffd720ec29a5ef7a4e8f85ed60329b34d51f09
Wave2055 <jaitinmoodally@outlook.com>
2026-07-13 22:04:28 +0200

    set auth tokens

---
+++ services/auth/cookies.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timezone
+
+from fastapi import Response
+
+from services.auth.auth_settings import AuthSettings
+
+
+def set_auth_cookies(
+	response: Response,
+	*,
+	settings: AuthSettings,
+	access_token: str,
+	refresh_token: str,
+	refresh_expires_at: datetime,
+) -> None:
+	access_max_age = settings.access_token_expire_minutes * 60
+	refresh_max_age = int((refresh_expires_at - datetime.now(timezone.utc)).total_seconds())
+	common = dict(
+		httponly=True,
+		secure=settings.cookie_samesite,
+		samesite=settings.cookie_samesite,
+		domain=settings.cookie_domain,
+		path='/',
+	)
+
+	response.set_cookie(settings.access_cookie_name, access_token, max_age=access_max_age, **common)
+
+	response.set_cookie(
+		settings.refresh_cookie_name, refresh_token, max_age=refresh_max_age, **common
+	)
