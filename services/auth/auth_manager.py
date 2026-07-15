b1bdcda8f3dc74a65db6388f66d9ee8ea0ed13b7
Wave2055 <jaitinmoodally@outlook.com>
2026-07-15 22:35:18 +0200

    auth manager signup and login functions

--- services/auth/auth_manager.py
+++ services/auth/auth_manager.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta, timezone
 from dataclass import dataclass
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from services.auth.password_service import hash_password
+from services.auth.password_service import hash_password, verify_password
 from services.auth.token_service import token_service
 from services.database_manager.managers.refresh_token_manager import refresh_token_manager
 from services.database_manager.managers.user_manager import user_manager
@@ -46,15 +46,25 @@ class AuthManager:
 
 		user = await user_manager.create(email=email, hashed_password=password_hash)
 
-		return await self._create_session(user)
+		return await self._create_session(user, db)
+
+	async def authenticate(self, *, db: AsyncSession, email: str, password: str) -> SessionTokens:
+		user = await user_manager.get_by_email(email=email)
+
+		if user is None or not verify_password(password=password, stored_hash=user.hashed_password):
+			raise InvalidCredentialsError()
+
+		return await self._create_session(user, db)
 
 	async def _create_session(self, user: User, db: AsyncSession) -> SessionTokens:
 
 		access_token = token_service.create_access_token(user.id)
-		refresh = token_service.create_access_token
+		refresh, hash = token_service.create_refresh_token()
 		expires = datetime.now(timezone.utc) + timedelta(hours=24)
 
-		await refresh_token_manager.create(db=db, user_id=user.id, expires_at=expires)
+		await refresh_token_manager.create(
+			db=db, user_id=user.id, expires_at=expires, token_hash=hash
+		)
 		return SessionTokens(
 			access_token=access_token, refresh_token=refresh, refresh_expires_at=expires
 		)
