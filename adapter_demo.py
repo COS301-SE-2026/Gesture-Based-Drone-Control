311ab602ea06a105b9a29bc57548395e8f4c45e5
Shavir <shavirvallabh05@gmail.com>
2026-07-16 00:31:16 +0200

    updated dronesim and telemetry classes

--- adapter_demo.py
+++ adapter_demo.py
@@ -273,6 +273,8 @@ class AppState:
 				'battery_pct': t.battery_pct,
 				'heading_deg': t.heading_deg,
 				'is_flying': t.is_flying,
+				'x_displacement': t.x_displacement,
+				'y_displacement': t.y_displacement,
 				'source': t.source,
 			}
 		except Exception as ex:
@@ -394,7 +396,7 @@ async def ws_keyboard(ws: WebSocket):
 	try:
 		while True:
 			msg = await ws.receive_json()
-			await state.keyboard_input.handle_message(msg)
+			state.keyboard_input.handle_message(msg)
 	except WebSocketDisconnect:
 		state.log('Keyboard WebSocket disconnected')
 
@@ -774,6 +776,16 @@ FRONTEND_HTML = """
         </div>
 
         <div class="telem-cell">
+          <div class="telem-label">X Displacement</div>
+          <div class="telem-value" id="t-x">-</div>
+        </div>
+
+        <div class="telem-cell">
+          <div class="telem-label">Y Displacement</div>
+          <div class="telem-value" id="t-y">-</div>
+        </div>
+
+        <div class="telem-cell">
           <div class="telem-label">Speed</div>
           <div class="telem-value" id="t-spd">-</div>
         </div>
@@ -939,6 +951,16 @@ function updateTelemetry(d) {
   setText(
     "t-alt",
     d.altitude_m !== undefined ? d.altitude_m.toFixed(2) : "-"
+  );
+
+  setText(
+    "t-x",
+    d.x_displacement !== undefined ? d.x_displacement.toFixed(2) : "-"
+  );
+
+  setText(
+    "t-y",
+    d.y_displacement !== undefined ? d.y_displacement.toFixed(2) : "-"
   );
 
   setText(
