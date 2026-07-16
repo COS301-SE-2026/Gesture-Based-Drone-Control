c6b6f5d0f0139c058a845288051a8241e940217c
Wave2055 <jaitinmoodally@outlook.com>
2026-07-16 12:25:27 +0200

    lets save the artifacts for the playwright failures

--- apps/frontend/playwright.unit.config.ts
+++ apps/frontend/playwright.unit.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -28,7 +28,9 @@ export default defineConfig({
      baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
@@ -48,7 +50,7 @@ export default defineConfig({
    webServer: [
     {
      command: 'yarn dev',
-     url: 'http://localhost:3000',
+     url: 'http://localhost:3000/',
      reuseExistingServer: !process.env.CI,
    }]
 
