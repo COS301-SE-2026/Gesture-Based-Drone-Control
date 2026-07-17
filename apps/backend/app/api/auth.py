836842917d5113441aa1fa07fb83bfb33c88f951
Wave2055 <jaitinmoodally@outlook.com>
2026-07-17 23:01:30 +0200

    minor fixes for PR

--- apps/backend/app/api/auth.py
+++ apps/backend/app/api/auth.py
@@ -91,7 +91,7 @@ async def refresh(request: RefreshRequest, response: Response, db: AsyncSession 
 
 		return AuthResponse(message='Token Refresh Successful')
 	except InvalidRefreshTokenError as e:
-		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, details=str(e))
+		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
 
 
 @router.post('/logout', response_model=AuthResponse, status_code=status.HTTP_200_OK)
@@ -100,5 +100,7 @@ async def logout(request: RefreshRequest, response: Response, db: AsyncSession =
 		await auth_manager.logout(db=db, refresh_token=request.refresh_token)
 
 		clear_auth_cookies(response=response)
+
+		return AuthResponse(message="Logout Successful")
 	except InvalidRefreshTokenError as e:
-		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, details=str(e))
+		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
