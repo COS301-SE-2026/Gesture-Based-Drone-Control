395cea3ec0dfc84e6bf2bb2d0a903febf040589b
Ayush-B99 <ayush.beekum@gmail.com>
2026-07-17 18:56:40 +0200

    init of landing page file setup

---
+++ landing_page/vite.config.ts
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
+  build: {
+    cssMinify: "esbuild",
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 3009,           // desired port
+    strictPort: false,    // falls back to next available if 3000 is taken
+
+    watch: {
+      usePolling: true,
+      interval: 100,   // milliseconds between polls (optional)
+    },
+  },
+    
+})
