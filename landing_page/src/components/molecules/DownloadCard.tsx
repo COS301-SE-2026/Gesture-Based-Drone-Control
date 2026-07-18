98862a012140c75030ffabd1f5f33c599f7a0c6a
Ayush-B99 <ayush.beekum@gmail.com>
2026-07-18 20:19:30 +0200

    molecules/Pipeline files complete

--- landing_page/src/components/molecules/DownloadCard.tsx
+++ landing_page/src/components/molecules/DownloadCard.tsx
@@ -4,26 +4,26 @@ import { RELEASES } from "../../constants/config"
 import "./DownloadCard.css"
 
 interface Props {
-    os: string
-    ext: string
-    req: string
-    delay?: number
+  os: string
+  ext: string
+  req: string
+  delay?: number
 }
 
-export default function DownloadCard({os, ext, req, delay=0}: Props) {
-    return (
-        <Reveal
-        as="a"
-        className="md-dlcard md-spot"
-        href={RELEASES}
-        delay={delay}
-        onMouseMove={spotlight}
-        >
-            <span className="md-dlext">{ext}</span>
-            <h3>{os}</h3>
-            <p>{req}</p>
-            {/* symbol pasted */}
-            <span className="md-dlbtn">Download v0.9.0 ↓</span>
-        </Reveal>
-    )
+export default function DownloadCard({ os, ext, req, delay = 0 }: Props) {
+  return (
+    <Reveal
+      as="a"
+      className="md-dlcard md-spot"
+      href={RELEASES}
+      delay={delay}
+      onMouseMove={spotlight}
+    >
+      <span className="md-dlext">{ext}</span>
+      <h3>{os}</h3>
+      <p>{req}</p>
+      {/* symbol pasted */}
+      <span className="md-dlbtn">Download v0.9.0 ↓</span>
+    </Reveal>
+  )
 }
