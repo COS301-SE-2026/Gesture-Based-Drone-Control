a1d65a08674d555568368b4305cffc01269397c5
Ayush-B99 <ayush.beekum@gmail.com>
2026-07-23 20:33:57 +0200

    landing page hosting setup

--- landing_page/vite.config.ts
+++ landing_page/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/Gesture-Based-Drone-Control/",
   build: {
     cssMinify: "esbuild",
   },
