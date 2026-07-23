4b347915d8397228961d3304d0192bf9f1ad1116
Jaitin <jaitinmoodally@outlook.com>
2026-07-24 01:52:23 +0200

    backend and frontend build is working

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -59,9 +59,14 @@ app = FastAPI(
 # lets Fastapi know that requests from lclhst3000 is chill
 app.add_middleware(
 	CORSMiddleware,
-	allow_origins=['http://localhost:3000'],
+	allow_origins=['http://localhost:3000', 'http://localhost:4173'],
 	allow_credentials=True,
 	allow_methods=['*'],
 	allow_headers=['*'],
 )
 app.include_router(router)
+
+if __name__ == '__main__':
+	import uvicorn
+
+	uvicorn.run(app, host='0.0.0.0', port=3001)
