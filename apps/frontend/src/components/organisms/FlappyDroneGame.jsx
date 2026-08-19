f17c31e17f1e301c3d62d32f62b87a4c744421a3
Shavir <shavirvallabh05@gmail.com>
2026-08-19 23:44:23 +0200

    checkpoint: something shows up. not the right thing, but something...

--- apps/frontend/src/components/organisms/FlappyDroneGame.jsx
+++ apps/frontend/src/components/organisms/FlappyDroneGame.jsx
@@ -59,12 +59,90 @@ export default function FlappyDroneGame() {
         })
 
         //controls here. will add handlers when integrated with backend stuffs
-        k.onKeyPress("up", () => player.jump(JUMP_FORCE))
-        k.onKeyPress("right", () => player.jump(0.00001))
-        k.onKeyPress("down", () => player.jump(-JUMP_FORCE))
+        k.onKeyPress("ArrowUp", () => player.jump(JUMP_FORCE))
+        k.onKeyPress("ArrowRight", () => player.jump(0.00001))
+        k.onKeyPress("ArrowDown", () => player.jump(-JUMP_FORCE))
+        
+        // we want to spawn pipes around the center
+        // take prev pipe into consideration so its not impossible
+        let prevPipeH1 = k.center().y - PIPE_OPEN / 2
+
+        // recalled to dynamically create pipes within a random window
+        function spawnPipe() {
+          const PIPE_MAX = k.height() - PIPE_MIN - PIPE_OPEN
+          const low = Math.max(PIPE_MIN, prevPipeH1 - PIPE_OPEN * 1.2)
+          const high = Math.min(PIPE_MAX, prevPipeH1 + PIPE_OPEN * 1.2)
+          const h1 = prevPipeH1 = k.rand(low, high)
+          const h2 = k.height() - h1 - PIPE_OPEN
+
+          // generic object template for pipes to follow
+          const makePipe = (posY, h) => [
+            k.pos(k.width(), posY),
+            k.rect(64, h),
+            k.color(10, 0, 33),
+            k.outline(4), //black outline on pixels
+            k.area({ isSensor: true}), //collision
+            k.move(k.LEFT, SPEED), //illusion of scrolling level
+            k.offscreen({destroy: true}), //it dont exist if its behind us
+            "pipe", //easier to refer to later on with a tag
+          ]
+
+          //make a top pipe
+          k.add(makePipe(0, h1))
+
+          //make a bottom pipe
+          k.add([...makePipe(h1 + PIPE_OPEN, h2), {passed: false}])
+        }
+
+        // lose condition
+        player.onCollide("pipe", () => k.go("lose", score))
+
+        // the pipe is actually moving not the player
+        // so when the pipe passes the player, give them a point
+        k.onUpdate("pipe", (p) => {
+          if (p.pos.x + p.width <= player.pos.x && !p.passed) {
+            score++
+            scoreLabel.text = score.toString()
+            p.passed = true
+          }
+        })
+
+        // spawn pipe every second
+        k.loop(1, spawnPipe)
+
+        let score = 0
+        const scoreLabel = k.add([
+          k.text("0", {size: 48}),
+          k.anchor("center"), // keep it in place
+          k.pos(k.width()/2, 80), //top centered
+          k.fixed(), //unaffected by camera 
+          k.z(1000), //big number because on top layer above all else
+        ])
       })
+         // the scene that shows when one crashes
+         k.scene("lose", (score = 0) => {
+          k.add([
+            k.text(`Score: ${score}`, { size: 48 }),
+            k.anchor("center"),
+            k.pos(k.width() / 2, k.height() / 2 - 40),
+            k.color(200,200,200),
+          ])
+          k.add([
+            k.text("Up arrow to retry", {size: 24}),
+            k.anchor("center"),
+            k.pos(k.width() / 2, k.height() / 2 + 40),
+            k.color(180, 180, 180),
+          ])
+          // option to retry 
+          k.wait(0.2, () => {
+            k.onKeyPress("ArrowUp", () => k.go("game"))
+            k.onMousePress(() => k.go("game"))
+          })
+         })
+
+         k.go("game")
     })
-  })
+  }, [])
 
   return (
     <canvas
